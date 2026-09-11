621189a4f0c0b5348d8f01ee66c97d7cb6ae4d89
a9sgg <awsy973@gmail.com>
2026-09-11 14:46:08 +0300

    Update AetherialOres.java

--- src/main/java/net/aetherialores/AetherialOres.java
+++ src/main/java/net/aetherialores/AetherialOres.java
@@ -1,36 +1,38 @@
 package net.a9sgg.aetherialores;
 
+import net.a9sgg.aetherialores.registry.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AetherialOres implements ModInitializer {
-    public static final String MOD_ID = "aetherial-ores";
 
-    // 1. تعريف بلوكة أور الأثيريل بخصائص Deepslate Diamond Ore
-    public static final Block AETHERIAL_ORE = new Block(AbstractBlock.Settings.copy(Blocks.DEEPSLATE_DIAMOND_ORE));
+	public static final String MOD_ID = "aetherial-ores";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    @Override
-    public void onInitialize() {
-        // 2. تسجيل البلوكة والعنصر في السجل البرمجي
-        Registry.register(Registries.BLOCK, Identifier.of(MOD_ID, "aetherial_ore"), AETHERIAL_ORE);
-        Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "aetherial_ore"), 
-            new BlockItem(AETHERIAL_ORE, new Item.Settings()));
+	@Override
+	public void onInitialize() {
+		LOGGER.info("[{}] Initializing...", MOD_ID);
 
-        // 3. إضافتها لقائمة البناء Natural / Building Blocks في الكريتيف
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
-            content.add(AETHERIAL_ORE);
-        });
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(content -> {
-            content.add(AETHERIAL_ORE);
-        });
-    }
+		// Register all blocks + block items
+		ModBlocks.registerBlocks();
+
+		// Add ore blocks to the "Natural" creative tab
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+			entries.add(ModBlocks.AETHERIUM_ORE);
+			entries.add(ModBlocks.VOIDSTEEL_ORE);
+			entries.add(ModBlocks.CHRONOCITE_ORE);
+		});
+
+		// Add refined blocks to the "Building Blocks" creative tab
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+			entries.add(ModBlocks.AETHERIUM_BLOCK);
+			entries.add(ModBlocks.VOIDSTEEL_BLOCK);
+			entries.add(ModBlocks.CHRONOCITE_BLOCK);
+		});
+
+		LOGGER.info("[{}] Initialization complete.", MOD_ID);
+	}
 }
